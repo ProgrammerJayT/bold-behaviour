f95348daa80cae772b7bb063585b987836ef35df
ProgrammerJayT <theanthem8@gmail.com>
2023-01-03 13:55:26 +0200

    Created images table attributes

--- database/migrations/2023_01_03_112323_create_images_table.php
+++ database/migrations/2023_01_03_112323_create_images_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('images', function (Blueprint $table) {
-            $table->id();
+            $table->integer('image_id')->autoIncrement();
+            $table->string('title');
+            $table->string('description');
+            $table->string('url');
             $table->timestamps();
         });
     }
